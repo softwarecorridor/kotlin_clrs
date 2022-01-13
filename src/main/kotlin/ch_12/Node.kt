283ce501d27d6a2c86a32f44aab3d32b7c1dbb6b
softwarecorridor <softwarecorridor@users.noreply.github.com>
2022-01-12 21:29:56 -0500

    add helpers for checking left / right children

--- src/main/kotlin/ch_12/Node.kt
+++ src/main/kotlin/ch_12/Node.kt
@@ -4,4 +4,12 @@ class Node {
     var left : Node? = null
     var right : Node? = null
     var value : Int? = null
+
+    fun hasRight() : Boolean {
+        return right != null
+    }
+
+    fun hasLeft(): Boolean {
+        return left != null
+    }
 }
