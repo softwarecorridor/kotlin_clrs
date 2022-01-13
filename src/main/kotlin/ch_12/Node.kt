2ccf514d6a1e9bb304d52730575e9b80913e504c
softwarecorridor <softwarecorridor@users.noreply.github.com>
2022-01-12 22:30:59 -0500

    add parent to Node

--- src/main/kotlin/ch_12/Node.kt
+++ src/main/kotlin/ch_12/Node.kt
@@ -1,15 +1,24 @@
 package ch_12
 
 class Node {
-    var left : Node? = null
-    var right : Node? = null
-    var value : Int? = null
+    var left: Node? = null
+    var right: Node? = null
+    var parent: Node? = null
+    var value: Int? = null
 
-    fun hasRight() : Boolean {
+    fun hasRight(): Boolean {
         return right != null
     }
 
     fun hasLeft(): Boolean {
         return left != null
+    }
+
+    fun hasParent(): Boolean {
+        return parent != null
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return (other is Node) && (other.left == left) && (other.right == right) && (other.parent == parent)
     }
 }
