95d764022d7c708edd0a7ca55f282675e9bfedba
softwarecorridor <softwarecorridor@users.noreply.github.com>
2022-01-12 21:47:56 -0500

    add iterative tree walk

--- src/main/kotlin/ch_12/main_ch12.kt
+++ src/main/kotlin/ch_12/main_ch12.kt
@@ -1,5 +1,7 @@
 package ch_12
 
+import kotlin.collections.ArrayDeque
+
 fun main(args: Array<String>) {
     val tree = Node()
     tree.value = 6
@@ -13,17 +15,16 @@ fun main(args: Array<String>) {
     tree.left!!.right!!.value = 5
     tree.right!!.right = Node()
     tree.right!!.right!!.value = 8
-    treeWalkInorder(tree)
-    val returnNode = treeSearch(tree, 8)
-    print(returnNode!!.value == 8)
+//    recursiveTreeWalkInorder(tree)
+    iterativeTreeWalkInorder(tree)
 }
 
 
-fun treeSearch(node : Node?, searchValue: Int): Node? {
-    if(node == null || searchValue == node.value){
+fun treeSearch(node: Node?, searchValue: Int): Node? {
+    if (node == null || searchValue == node.value) {
         return node
     }
-    return if ( searchValue < node.value!!) {
+    return if (searchValue < node.value!!) {
         treeSearch(node.left, searchValue)
     } else {
         treeSearch(node.right, searchValue)
@@ -31,10 +32,32 @@ fun treeSearch(node : Node?, searchValue: Int): Node? {
 
 }
 
-fun treeWalkInorder(node: Node?) {
-    if (node != null ) {
-        treeWalkInorder(node.left)
+fun recursiveTreeWalkInorder(node: Node?) {
+    if (node != null) {
+        recursiveTreeWalkInorder(node.left)
         print(node.value)
-        treeWalkInorder(node.right)
+        recursiveTreeWalkInorder(node.right)
+    }
+}
+
+// 12.1-3 - stack
+fun iterativeTreeWalkInorder(node: Node?) {
+    if (node != null) {
+        val stack = ArrayDeque<Node>()
+        stack.addFirst(node)
+        while (stack.first().hasLeft()) {
+            stack.addFirst(stack.first().left!!)
+        }
+
+        while (!stack.isEmpty()) {
+            val currentNode = stack.removeFirst()
+            print(currentNode.value)
+            if (currentNode.hasRight()) {
+                stack.addFirst(currentNode.right!!)
+                while (stack.first().hasLeft()) {
+                    stack.addFirst(stack.first().left!!)
+                }
+            }
+        }
     }
 }
